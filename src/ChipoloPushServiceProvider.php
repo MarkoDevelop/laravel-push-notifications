d7a43cdcb58769c32f66e008c6c100bda6f63988
Marko <marko.zagar@gmail.com>
2019-02-12 14:04:59 +0100

    Add basic class to service provider

---
+++ src/ChipoloPushServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Chipolo\Push;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ChipoloPushServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
