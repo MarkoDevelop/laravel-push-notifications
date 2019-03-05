108dc77391f054b6f09c9bf2f6eee28a2835618d
Marko <marko.zagar@gmail.com>
2019-03-05 10:01:25 +0100

    Change config file to use files from env

--- config/chipolo-push.php
+++ config/chipolo-push.php
@@ -5,12 +5,12 @@ return [
         'keep-alive' => true,
     ],
     'ios'     => [
-        'certificate-path' => base_path('key_chipolo.p8'),
+        'certificate-path' => env('PUSH_IOS_AUTH_KEY_PATH', base_path('key_chipolo.p8')),
         'secret'           => env('PUSH_IOS_SECRET'),
         'team-id'          => env('PUSH_IOS_TEAM_ID'),
     ],
     'android' => [
         'authorization-key'              => env('PUSH-ANDROID-AUTH-KEY'),
-        'google_application_credentials' => base_path('firebase_service_account_chipolo.json'),
+        'google_application_credentials' => env('GOOGLE_APPLICATION_CREDENTIALS', base_path('firebase_service_account_chipolo.json')),
     ],
 ];
